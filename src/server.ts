2b0fbaff3be7432b150425fce6cb71d45371437e
Upendra Kumar <toxic74412@gmail.com>
2024-11-29 21:22:43 +0530

    setup database

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,24 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import http from "http";
+import mongoose from "mongoose";
 import app from "./app";
 
 const PORT = process.env.PORT || 9001;
+const MONGO_URI = process.env.MONGO_URI || "";
+
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB connected successfully!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
+};
 
 const startServer = async () => {
+  await connectToMongoDB();
   const server = http.createServer(app);
   server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
