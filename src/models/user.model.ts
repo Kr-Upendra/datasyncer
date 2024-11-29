eb387d19f51afcb556b40fbd741515f1ac4c8e22
Upendra Kumar <toxic74412@gmail.com>
2024-11-29 21:25:22 +0530

    create user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      select: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export const UserModel = model("User", userSchema);
