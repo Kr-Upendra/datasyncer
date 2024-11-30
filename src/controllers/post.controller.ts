e829878a1390e8ad5b087bf220fa5b4234249dd6
Upendra Kumar <toxic74412@gmail.com>
2024-11-30 11:12:51 +0530

    implement pagination

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -35,11 +35,24 @@ export const createPost = asyncHandler(
 
 export const getAllPosts = asyncHandler(
   async (req: Request, res: Response<ResponsePayload>, next: NextFunction) => {
-    const posts = await PostModel.find();
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 2;
+    const offset = (page - 1) * limit;
+
+    const posts = await PostModel.find().skip(offset).limit(limit);
+
+    const total = await PostModel.countDocuments();
+
+    const pagination = {
+      totalRecords: total,
+      currentPage: page,
+      itemsPerPage: limit,
+    };
+
     res.status(200).json({
       status: "success",
       message: "all posts.",
-      data: { posts },
+      data: { posts, pagination },
     });
   }
 );
